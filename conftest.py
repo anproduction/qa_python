1bd006503335230b7e54404123518f621215dd0f
Anastasia Prodan <prodananastasija@yandex.com>
2025-04-23 19:53:49 +0300

    first_commit

---
+++ conftest.py
@@ -0,0 +1,28 @@
+import pytest
+from main import BooksCollector
+
+# Тестовые данные
+fantasy_book = 'Хроники Нарнии'
+horror_book = 'Оно'
+detective_book = 'Убийство в Восточном экспрессе'
+child_book = 'Малыш и Карлсон'
+comedy_book = 'Трое в лодке'
+long_name = 'Очень длинное название книги, которое явно превышает лимит в сорок символов'
+
+@pytest.fixture
+def collector():
+    return BooksCollector()
+
+@pytest.fixture
+def collector_with_books(collector):
+    books = {
+        fantasy_book: 'Фантастика',
+        horror_book: 'Ужасы',
+        detective_book: 'Детективы',
+        child_book: 'Детям',
+        comedy_book: 'Юмор'
+    }
+    for book, genre in books.items():
+        collector.add_new_book(book)
+        collector.set_book_genre(book, genre)
+    return collector
