e99421e52d888e67c62f92e486bb965e80b4e0cf
Anastasia Prodan <prodananastasija@yandex.com>
2025-04-26 13:06:40 +0300

    add_test_data

--- conftest.py
+++ conftest.py
@@ -1,13 +1,6 @@
 import pytest
 from main import BooksCollector
-
-# Тестовые данные
-fantasy_book = 'Хроники Нарнии'
-horror_book = 'Оно'
-detective_book = 'Убийство в Восточном экспрессе'
-child_book = 'Малыш и Карлсон'
-comedy_book = 'Трое в лодке'
-long_name = 'Очень очень очень длинное название книги, которое превышает лимит в сорок символов'
+from test_data import books_genres
 
 @pytest.fixture
 def collector():
@@ -15,14 +8,7 @@ def collector():
 
 @pytest.fixture
 def collector_with_books(collector):
-    books = {
-        fantasy_book: 'Фантастика',
-        horror_book: 'Ужасы',
-        detective_book: 'Детективы',
-        child_book: 'Мультфильмы',
-        comedy_book: 'Комедии'
-    }
-    for book, genre in books.items():
+    for book, genre in books_genres.items():
         collector.add_new_book(book)
         collector.set_book_genre(book, genre)
     return collector
